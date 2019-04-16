91e93b7c6fa513ad6b5b8bf38bb21eb207fb66d7
bellawn99 <bellawulan99@mail.ugm.ac.id>
2019-04-16 19:23:36 +0700

    Updates

---
+++ precache-manifest.1f237ae465aa65ab91db4ba330a75b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "d3e6f8bca6dafe650a15",
+    "url": "/react-gh-pages/static/js/main.03bb0320.chunk.js"
+  },
+  {
+    "revision": "2ec2c2c6f8c64a62f5e9",
+    "url": "/react-gh-pages/static/js/2.2f459df8.chunk.js"
+  },
+  {
+    "revision": "d3e6f8bca6dafe650a15",
+    "url": "/react-gh-pages/static/css/main.61a25ec0.chunk.css"
+  },
+  {
+    "revision": "d566adf8a4beb3b9309ed9266d4dc706",
+    "url": "/react-gh-pages/index.html"
+  }
+];
