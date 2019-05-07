a75ce4b45a3ddcb547da0ba9a11b4909eea2f69d
bellawn99 <bellawulan99@mail.ugm.ac.id>
2019-05-07 10:00:41 +0700

    first commit

---
+++ src/componen/page/Portofolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Portofolio extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Profile page</h1>
+            </div>
+        )
+    }
+}
+
+export default Portofolio;
